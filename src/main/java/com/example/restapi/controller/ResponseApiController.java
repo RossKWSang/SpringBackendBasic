5631e714419aa493b9eada75e67ec76e2ee5bf09
Wonsang Kim <rosskysoss@gmail.com>
2023-10-19 22:22:21 +0900

    response process

--- src/main/java/com/example/restapi/controller/ResponseApiController.java
+++ src/main/java/com/example/restapi/controller/ResponseApiController.java
@@ -1,22 +1,34 @@
 package com.example.restapi.controller;
 
 import com.example.restapi.model.UserRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class ResponseApiController {
 
     // http://localhost:8080/api/v1
     @GetMapping("")
-    public UserRequest user() {
+    public ResponseEntity<UserRequest> user() {
         var user = new UserRequest();
         user.setUserName("홍길동");
         user.setUserAge(10);
         user.setEmail("hong@gmail.com");
-        return user;
+
+        log.info("user: {}", user);
+
+        var response = ResponseEntity
+                .status(HttpStatus.OK)
+                .header("x-custom", "hi")
+                .body(user);
+
+        return response;
     }
 }
