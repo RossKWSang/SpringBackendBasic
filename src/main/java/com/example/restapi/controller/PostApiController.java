ab99bcfa3a4dd904516eb1d4f10a6dbc69ccca58
Wonsang Kim <rosskysoss@gmail.com>
2023-10-19 09:36:00 +0900

    feat: add POST method

--- src/main/java/com/example/restapi/controller/PostApiController.java
+++ src/main/java/com/example/restapi/controller/PostApiController.java
@@ -1,6 +1,7 @@
 package com.example.restapi.controller;
 
 import com.example.restapi.model.BookRequest;
+import com.example.restapi.model.MemberRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,4 +20,10 @@ public class PostApiController {
 
 
     // TODO RequestBody로 사용자의 이름, 전화번호, 이메일을 받는 POST Method를 만들기
+    @PostMapping("/member")
+    public MemberRequest member(
+            @RequestBody MemberRequest memberRequest
+    ) {
+        return memberRequest;
+    }
 }
