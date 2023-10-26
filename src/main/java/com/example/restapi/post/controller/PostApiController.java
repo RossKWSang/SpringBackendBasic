35b2198db40f7c1bacf68add0cd94346b0e4e364
Wonsang Kim <rosskysoss@gmail.com>
2023-10-26 19:59:20 +0900

    view and delete

--- src/main/java/com/example/restapi/post/controller/PostApiController.java
+++ src/main/java/com/example/restapi/post/controller/PostApiController.java
@@ -2,14 +2,13 @@ package com.example.restapi.post.controller;
 
 import com.example.restapi.post.db.PostEntity;
 import com.example.restapi.post.model.PostRequest;
+import com.example.restapi.post.model.PostViewRequest;
 import com.example.restapi.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/post")
@@ -24,5 +23,30 @@ public class PostApiController {
         PostRequest postRequest
     ) {
         return postService.create(postRequest);
+    }
+
+    @PostMapping("view")
+    public PostEntity view(
+            @Valid
+            @RequestBody
+            PostViewRequest postViewRequest
+    ) {
+        return postService.view(postViewRequest);
+    }
+
+    @GetMapping("/all")
+    public List<PostEntity> list(
+
+    ) {return postService.all();
+
+    }
+
+    @PostMapping("/delete")
+    public void delete(
+            @Valid
+            @RequestBody
+            PostViewRequest postViewRequest
+    ) {
+        postService.delete(postViewRequest);
     }
 }
--- src/main/java/com/example/restapi/post/service/PostService.java
+++ src/main/java/com/example/restapi/post/service/PostService.java
@@ -3,11 +3,13 @@ package com.example.restapi.post.service;
 import com.example.restapi.post.db.PostEntity;
 import com.example.restapi.post.db.PostRepository;
 import com.example.restapi.post.model.PostRequest;
+import com.example.restapi.post.model.PostViewRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +31,46 @@ public class PostService {
                 .build();
 
         return postRepository.save(entity);
+    }
+
+    /* 1. 게시글이 있는가?
+    /  2. 비밀번호가 맞는가?
+     */
+    public PostEntity view(PostViewRequest postViewRequest) {
+        return postRepository.findFirstByIdAndStatusOrderByIdDesc(postViewRequest.getPostId(), "REGISTERED")
+                .map( it -> {
+                    if(!it.getPassword().equals(postViewRequest.getPassword())){
+                        var format = "패스워드가 맞지 않습니다 %s vs %s";
+
+                        throw new RuntimeException(String.format(format, it.getPassword(), postViewRequest.getPassword()));
+                    }
+                    return it;
+                        }).orElseThrow(
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다 : " + postViewRequest.getPostId());
+                        }
+                );
+    }
+
+    public List<PostEntity> all() {
+        return postRepository.findAll();
+    }
+
+    public void delete(PostViewRequest postViewRequest) {
+        postRepository.findById(postViewRequest.getPostId())
+                .map( it -> {
+                    if(!it.getPassword().equals(postViewRequest.getPassword())){
+                        var format = "패스워드가 맞지 않습니다 %s vs %s";
+
+                        throw new RuntimeException(String.format(format, it.getPassword(), postViewRequest.getPassword()));
+                    }
+                    it.setStatus("UNREGISTERED");
+                    postRepository.save(it);
+                    return it;
+                }).orElseThrow(
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다 : " + postViewRequest.getPostId());
+                        }
+                );
     }
 }
