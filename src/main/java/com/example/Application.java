e3b905d9791d916bfe80f8f1e8d038bba9ae5b8f
Wonsang Kim <rosskysoss@gmail.com>
2023-10-28 12:02:27 +0900

    authorization

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.session.SessionApplication;
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SessionApplication.class, args);
+    }
+
+}
