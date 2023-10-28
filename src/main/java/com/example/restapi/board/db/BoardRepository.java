3bae1cd9576e1b6cdfca7c70b6efbb39aef83b21
Wonsang Kim <rosskysoss@gmail.com>
2023-10-28 18:58:23 +0900

    multi-module setup

--- src/main/java/com/example/restapi/board/db/BoardRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.restapi.board.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-}
