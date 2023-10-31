2e33c790de856a1f1b78284a59930a6c8d9903b9
Wonsang Kim <rosskysoss@gmail.com>
2023-10-31 17:31:16 +0900

    api filter and common stack

--- api/src/main/java/com/example/api/account/AccountApiController.java
+++ api/src/main/java/com/example/api/account/AccountApiController.java
@@ -1,6 +1,7 @@
 package com.example.api.account;
 
 import com.example.api.account.model.AccountMeResponse;
+import com.example.api.common.api.Api;
 import com.example.db.account.AccountEntity;
 import com.example.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +19,14 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse save() {
-        return AccountMeResponse.builder()
+    public Api<AccountMeResponse> save() {
+        var response = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("A@gmail.com")
                 .registeredAt(LocalDateTime.now()) //"2023-10-28T20:59:35.587852" => ISO8601 Spec
                 .build();
+
+        return Api.OK(response);
     }
 }
 
