7458da98a825d9848e9c761db5c87e695d7967c4
Wonsang Kim <rosskysoss@gmail.com>
2023-11-01 16:39:52 +0900

    exception

--- api/src/main/java/com/example/api/account/AccountApiController.java
+++ api/src/main/java/com/example/api/account/AccountApiController.java
@@ -2,10 +2,12 @@ package com.example.api.account;
 
 import com.example.api.account.model.AccountMeResponse;
 import com.example.api.common.api.Api;
+import com.example.api.common.error.ErrorCode;
 import com.example.api.common.error.UserErrorCode;
 import com.example.db.account.AccountEntity;
 import com.example.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,14 +22,18 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public Api<Object> save() {
+    public Api<AccountMeResponse> me() {
         var response = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("A@gmail.com")
                 .registeredAt(LocalDateTime.now()) //"2023-10-28T20:59:35.587852" => ISO8601 Spec
                 .build();
 
-        return Api.ERROR(UserErrorCode.USER_NOT_FOUND, "홍길동이라는 사용자 없음");
+        var str = "안녕하세요";
+        var age = 0;
+        Integer.parseInt(str);
+
+        return Api.OK(response);
     }
 }
 
